7ca41e97773d73869d8ca7fdae5992803c0f0b22
aminulmhub <aminulmhub@gmail.com>
2023-10-05 19:10:00 +0600

    publish issue, innspub journal done

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,6 @@
 
 // announcements swiper
 const announcements = new Swiper('.announcements', {
-    loop: true,
     autoplay: true,
     autoplay: { 
         delay: 3000,  
@@ -90,7 +89,105 @@ const announcements = new Swiper('.announcements', {
 
 // global_editor swiper
 const global_editor = new Swiper('.global_editor', {
-    loop: true,
+    autoplay: true,
+    autoplay: { 
+        delay: 3000,  
+        disableOnInteraction: false,
+    },
+    slidesPerView: 1.3,
+    centeredSlides: true,
+    spaceBetween: 10,
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 576px
+        576: {
+        slidesPerView: 2,
+        centeredSlides: false,
+        spaceBetween: 20
+        },
+        // when window width is >= 992px
+        992: {
+        slidesPerView: 3,
+        centeredSlides: false,
+        spaceBetween: 30
+        },
+        // when window width is >= 1200px
+        1400: {
+        slidesPerView: 4,
+        centeredSlides: false,
+        spaceBetween: 40
+        }
+    },
+
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination', 
+        slidesPerView: 'auto', 
+        initialSlide: 1, 
+        resistanceRatio: 0, 
+        clickable: true 
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+// published_issue swiper
+const published_issue = new Swiper('.published_issue', {
+    autoplay: true,
+    autoplay: { 
+        delay: 3000,  
+        disableOnInteraction: false,
+    },
+    slidesPerView: 1.3,
+    centeredSlides: true,
+    spaceBetween: 10,
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 576px
+        576: {
+        slidesPerView: 2,
+        centeredSlides: false,
+        spaceBetween: 20
+        },
+        // when window width is >= 992px
+        992: {
+        slidesPerView: 3,
+        centeredSlides: false,
+        spaceBetween: 30
+        },
+        // when window width is >= 1200px
+        1400: {
+        slidesPerView: 4,
+        centeredSlides: false,
+        spaceBetween: 40
+        }
+    },
+
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination', 
+        slidesPerView: 'auto', 
+        initialSlide: 1, 
+        resistanceRatio: 0, 
+        clickable: true 
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+// published_issue swiper
+const innspub_journal = new Swiper('.innspub_journal', {
     autoplay: true,
     autoplay: { 
         delay: 3000,  
