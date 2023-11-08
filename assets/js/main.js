cb3657c6331f7d700967372563f2c48f8f761789
aminulds <aminulds61@gmail.com>
2023-11-08 18:06:00 +0600

    journal page continue

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,13 +287,56 @@ const innspub_journal = new Swiper('.innspub_journal', {
 });
 
 //   Text Truncate
-// requires jquery
 $(document).ready(function () {
   (function () {
     var showChar = 780;
     var ellipsestext = "...";
 
     $(".truncate").each(function () {
+      var content = $(this).html();
+      if (content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content;
+        var html =
+          '<div class="truncate-text" style="display:block">' +
+          c +
+          '<span class="moreellipses">' +
+          ellipsestext +
+          '&nbsp;&nbsp;<a href="" class="moreless more">Read more</a></span></span></div><div class="truncate-text" style="display:none">' +
+          h +
+          '<a href="" class="moreless less">Read less</a></span></div>';
+
+        $(this).html(html);
+      }
+    });
+
+    $(".moreless").click(function () {
+      var thisEl = $(this);
+      var cT = thisEl.closest(".truncate-text");
+      var tX = ".truncate-text";
+
+      if (thisEl.hasClass("less")) {
+        cT.prev(tX).toggle();
+        cT.slideToggle();
+      } else {
+        cT.toggle();
+        cT.next(tX).fadeToggle();
+      }
+      return false;
+    });
+    /* end iffe */
+  })();
+
+  /* end ready */
+});
+
+// journal_description truncate
+$(document).ready(function () {
+  (function () {
+    var showChar = 260;
+    var ellipsestext = "...";
+
+    $(".journal__description").each(function () {
       var content = $(this).html();
       if (content.length > showChar) {
         var c = content.substr(0, showChar);
