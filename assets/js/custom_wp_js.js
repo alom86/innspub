e4b9359861eb7ca0581b7ee257f15ce852c6457c
aminulds <aminulds61@gmail.com>
2023-12-04 16:47:07 +0600

    current issue created

--- assets/js/custom_wp_js.js
+++ assets/js/custom_wp_js.js
@@ -13,6 +13,16 @@
   $('#header-menu .current-menu-ancestor').addClass('active');
   $('#header-menu .current-menu-item').addClass('active');
 
-
+  // home nav-tab to select
+  if ($(window).width() > 767) {
+    $(".nav_tabs .nav").removeClass('dropdown-menu');
+    $(".nav_tabs .tab_select").removeClass('tab_select');
+  }
+  if ($(window).width() < 768){
+    $(document).on("click", '.dropdown-menu .nav-item .nav-link', function(e) {
+      let activeText = $(this).text();
+      $(this).parent().parent().parent().find("button[data-bs-toggle='dropdown']").html(activeText);
+    });
+  }
 
 })(window.jQuery);
