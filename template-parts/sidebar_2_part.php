b6afea548e0ca62d58d5392e61d0cab530c95b26
aminulds <aminulds61@gmail.com>
2023-12-28 13:38:28 +0600

    most popular post functionality dynamic

--- template-parts/sidebar_2_part.php
+++ template-parts/sidebar_2_part.php
@@ -19,49 +19,69 @@
                 </h3>
                 <div id="most_popular" class="accordion-collapse collapse show" data-bs-parent="#accordionSidebar">
                     <div class="accordion-body">
-                        <div class="card_sidebar">
-                            <h4>
-                                <a href="#">Level of awareness and practices on solid waste</a>
-                            </h4>
-                            <div class="card_sidebar__meta">
-                                <span class="card_sidebar__label">JBES</span>
-                                <span class="card_sidebar__date">January 1, 2019</span>
-                            </div>
-                        </div>
-                        <div class="card_sidebar">
-                            <h4>
-                                <a href="#">Pineapple juice supplementation activates thyroid gland and attenuates</a>
-                            </h4>
-                            <div class="card_sidebar__meta">
-                                <span class="card_sidebar__label">IJBB</span>
-                                <span class="card_sidebar__date">January 1, 2019</span>
-                            </div>
-                        </div>
-                        <div class="card_sidebar">
-                            <h4>
-                                <a href="#">Seedling size and cotyledon retention have important influences</a>
-                            </h4>
-                            <div class="card_sidebar__meta">
-                                <span class="card_sidebar__label">IJAAR</span>
-                                <span class="card_sidebar__date">January 1, 2019</span>
-                            </div>
-                        </div>
-                        <div class="card_sidebar">
-                            <h4>
-                                <a href="#">Pakistan food safety challenges</a>
-                            </h4>
-                            <div class="card_sidebar__meta">
-                                <span class="card_sidebar__label">IJMM</span>
-                                <span class="card_sidebar__date">January 1, 2019</span>
-                            </div>
-                        </div>
+
+                        <?php
+                        // Specify the category slugs
+                        $category_slugs = array('ijb', 'ijbb', 'jbes', 'ijaar', 'ijmm');
+
+                        foreach ($category_slugs as $category_slug) {
+                            // Set up query arguments to fetch the most popular post in the current category
+                            $args = array(
+                                'category_name' => $category_slug,
+                                'posts_per_page' => 1, // Fetch only one post per category
+                                'meta_key' => 'post_views_count',
+                                'orderby' => 'meta_value_num',
+                                'order' => 'DESC'
+                            );
+
+                            $query = new WP_Query($args);
+
+                            if ($query->have_posts()) :
+                                while ($query->have_posts()) : $query->the_post();
+                                    // Display your post content here
+                        ?>
+                                    <div class="card_sidebar">
+                                        <h4>
+                                            <a href="<?php the_permalink() ?>">
+                                                <?php
+                                                $title = get_the_title();
+                                                $p_title = wp_trim_words($title, 8, ' ');
+                                                echo $p_title;
+                                                ?>
+                                            </a>
+                                        </h4>
+                                        <div class="card_sidebar__meta">
+                                            <span class="card_sidebar__label">
+                                                <?php
+                                                $categories = get_the_category();
+                                                if (!empty($categories)) {
+                                                    $category_link = get_category_link($categories[0]->term_id);
+                                                    echo '<a class="text-white" href="' . esc_url($category_link) . '">' . esc_html($categories[0]->name) . '</a>';
+                                                }
+                                                ?>
+                                            </span>
+                                            <span class="card_sidebar__date"><?php echo get_the_date('F j, Y') ?></span>
+                                        </div>
+                                    </div>
+                        <?php
+                                endwhile;
+                            endif;
+                            wp_reset_postdata(); // Reset post data for the next category
+                        }
+                        ?>
+
+
+
+
+
+
                     </div>
                 </div>
             </div>
 
             <!-- logo_menu -->
             <?php
-                require get_template_directory() . '/template-parts/logo_menu.php';
+            require get_template_directory() . '/template-parts/logo_menu.php';
             ?>
 
         </div>
